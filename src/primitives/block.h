62dec91f55f159a0147aa8deaec1f32677042b00
Andrew Tookay <andrewtookay@users.noreply.github.com>
2020-11-16 19:31:12 +0200

    remove incorrect extern declaration

--- src/primitives/block.h
+++ src/primitives/block.h
@@ -14,8 +14,6 @@
 #include "uint256.h"
 #include "utilstrencodings.h"
 
-extern BlockMap mapBlockIndex;
-
 /** Nodes collect new transactions into a block, hash them into a hash tree,
  * and scan through nonce values to make the block's hash satisfy proof-of-work
  * requirements.  When they solve the proof-of-work, they broadcast the block
